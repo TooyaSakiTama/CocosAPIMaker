93262625236361854e43a2e0f1a4d3d841143ed2
Saki <TooyaSakiTama@outlook.com>
2019-05-30 02:51:11 +0800

    类结构体->EmmyLua专用注释的转换完成 完善StringExtension的光标功能在使用NewLine的时候 会自动移动光标

--- CocosAPIMaker/EX/StringExtension.cs
+++ CocosAPIMaker/EX/StringExtension.cs
@@ -78,10 +78,6 @@ namespace System
     public static class StringBuilderEx
     {
         public static string cursor = "[cursor]";
-        public static StringBuilder NewLine(this StringBuilder sb)
-        {
-            return sb.Remove(cursor).AppendLine(cursor);
-        }
         public static StringBuilder RemoveCursor(this StringBuilder sb)
         {
             return sb.Remove(cursor);
@@ -92,34 +88,103 @@ namespace System
         }
         public static StringBuilder AddCursor(this StringBuilder sb,int line)
         {
-            return sb.Insert(sb.FindLine(line + 1) - 1, cursor);
+            return sb.Insert(sb.FindLineIndex(line + 1), cursor);
+        }
+        public static StringBuilder AddCursorIndex(this StringBuilder sb,int index)
+        {
+            return sb.Insert(index, cursor);
+        }
+        public static StringBuilder MoveCursor(this StringBuilder sb,int line)
+        {
+            return sb.RemoveCursor().AddCursor(line);
+        }
+        private static int FindCursorIndex(this StringBuilder sb)
+        {
+            int index = sb.ToString().IndexOf(cursor);
+            if (index == -1)
+            {
+                index = 0;
+            }
+            return index;
+        }
+        private static int FindCursorLine(this StringBuilder sb)
+        {
+            return sb.FindIndexLine(sb.FindCursorIndex());
+        }
+        public static int GetLineLength(this StringBuilder sb, int line)
+        {
+            int lineIndex = sb.FindLineIndex(line);
+            int afterLineIndex = sb.FindLineIndex(line + 1);
+            int length = afterLineIndex - lineIndex;
+            return length;
+        }
+        public static int GetLinesCount(this StringBuilder sb)
+        {
+            int line = 0;
+            if (string.IsNullOrEmpty(sb.ToString()))
+            {
+                return line;
+            }
+            line = sb.ToString().Split(Environment.NewLine).Length - 1;
+            return line;
+        }
+        public static StringBuilder MoveCursorToLineStart(this StringBuilder sb)
+        {
+            int cursorLine = sb.FindIndexLine(sb.FindCursorIndex());
+            int index = sb.FindLineIndex(cursorLine);
+            sb.RemoveCursor().AddCursorIndex(index);
+            return sb;
+        }
+        public static StringBuilder MoveCursorToLineEnd(this StringBuilder sb)
+        {
+            int cursorLine = sb.FindCursorLine();
+            return sb.RemoveCursor().AddCursor(cursorLine);
+        }
+        /// <summary>
+        /// 获取StringBuilder的字符串,这个方法可以避免装箱操作,但是光标会被删除,如果不想删除光标可以直接使用ToString方法
+        /// </summary>
+        /// <returns></returns>
+        public static string GetString(this StringBuilder sb)
+        {
+            sb.RemoveCursor();
+            return sb.ToString();
+        }
+
+        public static StringBuilder NewLine(this StringBuilder sb)
+        {
+            int line = sb.FindCursorLine();
+            int index = sb.FindCursorIndex();
+            return sb.RemoveCursor().Insert(index, Environment.NewLine).AddCursor(line + 1);
+        }
+        public static StringBuilder NewLine(this StringBuilder sb, int line)
+        {
+            return sb.RemoveCursor().Insert(sb.FindLineIndex(line), Environment.NewLine).AddCursor(line + 1);
         }
         public static StringBuilder NewLine(this StringBuilder sb,string value)
         {
-
-            return sb.RemoveCursor().AppendLine(value).AddCursor();
-        }
-        public static StringBuilder NewLine(this StringBuilder sb,int line)
-        {
-            return sb.RemoveCursor().Insert(sb.FindLine(line), Environment.NewLine).AddCursor(line);
+            int line = sb.FindCursorLine();
+            int index = sb.FindCursorIndex();
+            return sb.RemoveCursor().Insert(index, $"{Environment.NewLine}{value}").AddCursor(line + 1);
         }
         public static StringBuilder NewLine(this StringBuilder sb, int line,string value)
         {
-            sb.RemoveCursor().Insert(sb.FindLine(line), $"{Environment.NewLine}{value}").AddCursor(line);
+            sb.RemoveCursor().Insert(sb.FindLineIndex(line), $"{Environment.NewLine}{value}").AddCursor(line + 1);
             return sb;
         }
         public static StringBuilder Add(this StringBuilder sb, string value)
         {
-            return sb.RemoveCursor().Append(value).AddCursor();
+            int line = sb.FindCursorLine();
+            int index = sb.FindCursorIndex();
+            return sb.RemoveCursor().Insert(index,value).AddCursor(line + 1);
         }
         public static StringBuilder Add(this StringBuilder sb,int line, string value)
         {
-            sb.RemoveCursor().Insert(sb.FindLine(line), value).AddCursor(line);
+            int index = sb.FindCursorIndex();
+            sb.RemoveCursor().Insert(index, value).AddCursor(line);
             return sb;
         }
         public static StringBuilder Remove(this StringBuilder sb,string value)
         {
-            Console.WriteLine(sb.ToString().IndexOf(value));
             int startIndex = sb.ToString().IndexOf(value);
             if (startIndex == -1)
             {
@@ -129,21 +194,38 @@ namespace System
         }
         public static int FindLine(this StringBuilder sb,string s)
         {
+            return sb.FindIndexLine(sb.ToString().IndexOf(s));
+        }
+        private static int FindIndexLine(this StringBuilder sb, int index)
+        {
             int line = 0;
-            
+            int count = 0;
+            CharEnumerator sce = sb.ToString().GetEnumerator();
+            while (sce.MoveNext())
+            {
+                if (count == index)
+                {
+                    break;
+                }
+                if (sce.Current.ToString() == "\r")
+                {
+                    line++;
+                }
+                count++;
+            }
             return line;
         }
-        private static int FindLine(this StringBuilder sb, int byIndex)
+        private static int FindLineIndex(this StringBuilder sb, int line)
         {
             int index = 0;
             int count = 0;
             CharEnumerator sce = sb.ToString().GetEnumerator();
             while (sce.MoveNext())
             {
-                if (sce.Current.ToString() == Environment.NewLine)
+                if (sce.Current.ToString() == "\r")
                 {
                     count++;
-                    if (count == byIndex)
+                    if (count == line)
                     {
                         break;
                     }
--- CocosAPIMaker/TransDoc.cs
+++ CocosAPIMaker/TransDoc.cs
@@ -8,12 +8,49 @@ namespace CocosAPIMaker
 {
     class TransDoc
     {
-        private string newLine = "\n";
+        private string className = string.Empty;
         public string Trans(TransClass.ClassStruct _classStruct)
         {
-            string doc = "";
-            CreateClassDoc(_classStruct);
-            return doc;
+            className = _classStruct._Class;
+            StringBuilder sb = new StringBuilder(CreateClassDoc(_classStruct));
+            foreach (var function in _classStruct._Functions)
+            {
+                sb.Add(CreateFunctionDoc(function));
+                sb.MoveCursor(sb.GetLinesCount());
+            }
+            return sb.GetString();
+        }
+        private string CreateFunctionDoc(TransClass.FunctionStruct _functionStruct)
+        {
+            StringBuilder _functionDoc = new StringBuilder();
+            if (_functionStruct._Doc != null)
+            {
+                _functionDoc.NewLine(_functionStruct._Doc);
+            }
+            if (_functionStruct._Params != null)
+            {
+                foreach (var param in _functionStruct._Params)
+                {
+                    _functionDoc.NewLine(CreateParamDoc(param));
+                }
+
+            }
+            _functionDoc.NewLine(CreateReturnDoc(_functionStruct._Return));
+            _functionDoc.NewLine($"function {className}:{_functionStruct._Function} end");
+            _functionDoc.NewLine();
+            return _functionDoc.GetString();
+        }
+        private string CreateParamDoc(TransClass.ParamStruct _paramStruct)
+        {
+            StringBuilder _paramDoc = new StringBuilder();
+            _paramDoc.Add($"---@param {_paramStruct._Param} {_paramStruct._Type} {_paramStruct._Doc}");
+            return _paramDoc.GetString();
+        }
+        private string CreateReturnDoc(TransClass.ReturnStruct _returnStruct)
+        {
+            StringBuilder _returnDoc = new StringBuilder();
+            _returnDoc.Add($"---@return {_returnStruct._ReturnType} {_returnStruct._ReturnDoc}");
+            return _returnDoc.GetString();
         }
         private string CreateClassDoc(TransClass.ClassStruct _classStruct)
         {
@@ -22,12 +59,10 @@ namespace CocosAPIMaker
             _classDoc.NewLine($"---@class {_classStruct._Class} : {_classStruct._InheritedClass}");
             _classDoc.NewLine($"local {_classStruct._Class} = {{}}");
             _classDoc.NewLine($"{_classStruct._ParentModule}.{_classStruct._Class} = {_classStruct._Class}");
-            _classDoc.NewLine();
             _classDoc.NewLine($"return {_classStruct._Class}");
-            Console.WriteLine(_classDoc.ToString().GetLinesCount());
-            Console.WriteLine(_classDoc);
-            _classDoc.Add()
-            Console.WriteLine()
+            int line = _classDoc.FindLine($"{_classStruct._ParentModule}.{_classStruct._Class} = {_classStruct._Class}");
+            _classDoc.MoveCursor(line);
+            _classDoc.NewLine();
             return _classDoc.ToString();
         }
     }
